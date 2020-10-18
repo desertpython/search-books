90c8807a6d113e074d27558de35a6ae8285a0d78
Desert Python <mattsenneff@aol.com>
2020-10-17 19:19:19 -0700

    mutation login

---
+++ Develop/server/schemas/resolvers.js
@@ -0,0 +1,39 @@
+const { AuthenticationError } = require('apollo-server-express');
+const {
+    User
+} = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, {
+            _id
+        }) => {
+            return User.findOne({
+                _id
+            })
+        },
+        me: async (parent , args, context) => {
+            return User.findOne({
+                _id
+            })
+        },
+
+
+    },
+    Mutation: {
+        login: async (parent, {email , password}) => {
+            const user = await User.findOne(email)
+            if (!user) {
+                throw new AuthenticationError('Invalid login') 
+                
+            }
+            const correctpw = user.isCorrectPassword(password)
+            if (!correctpw) {
+                throw new AuthenticationError('Invalid login')
+            }
+            const token = signToken(user)
+            return {token , user}
+        }
+    }
+}
