da0209e8c11b846b861f1f3fcd0df4af43003c44
Desert Python <mattsenneff@aol.com>
2020-10-18 19:47:42 -0700

    new me query resolver

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -6,14 +6,17 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        me: async (parent , args, context) => {
-            return User.findOne({
-                _id
-            })
-        },
-
-
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+  
+          return userData;
+        }
+  
+        throw new AuthenticationError('Not logged in');
+      },
     },
+  
     Mutation: {
         login: async (parent, {email , password}) => {
             const user = await User.findOne(email)
