d66037011701ccb33f9b450150fe61fe771ab6d2
Desert Python <mattsenneff@aol.com>
2020-10-18 19:59:33 -0700

    removebook mutation

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -50,7 +50,18 @@ const resolvers = {
             }
       
             throw new AuthenticationError('You need to be logged in!');
-          },
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+              const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+              );
+      
+              return updatedUser;
+        }
+    }
       
     }
 }
